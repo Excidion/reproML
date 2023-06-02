04655341f2ada9534c5a9ad92eccc7e88cceaa02
Excidion <36995046+Excidion@users.noreply.github.com>
2023-06-02 16:53:01 +0200

    infer docs order from src

--- docs/helper/generate_code_docs.py
+++ docs/helper/generate_code_docs.py
@@ -22,6 +22,7 @@ for path in sorted(Path("src").rglob("*.py")):
     with mkdocs_gen_files.open(full_doc_path, "w") as fd:
         ident = ".".join(parts)
         fd.write(f"::: src.{ident}")
+        fd.write("\n\toptions:\n\t\tmembers_order: 'source'")  # order docs like source
 
     mkdocs_gen_files.set_edit_path(full_doc_path, path)
 
